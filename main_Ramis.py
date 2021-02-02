103c9359e35ee78d2f5f0356b514483c0651a824
fiafob <8D13jk37Op80EnD>
2021-02-02 17:34:12 +0500

    Rajkfa

---
+++ main_Ramis.py
@@ -0,0 +1,73 @@
+import os
+import sys
+
+import pygame
+import requests
+
+
+def get_pic():
+    global map_file, scale
+    map_request = f"http://static-maps.yandex.ru/1.x/?ll=136.274000,-25.596000&spn={scale},{scale}&l=map"
+    response = requests.get(map_request)
+
+    if not response:
+        print("Ошибка выполнения запроса:")
+        print(map_request)
+        print("Http статус:", response.status_code, "(", response.reason, ")")
+        sys.exit(1)
+
+    map_file = "map.png"
+    with open(map_file, "wb") as file:
+        file.write(response.content)
+
+
+def plus_spn():
+    global scale, map_file
+    scale += 3
+    map_request = f"http://static-maps.yandex.ru/1.x/?ll=136.274000,-25.596000&spn={scale},{scale}&l=map"
+    response = requests.get(map_request)
+
+    if not response:
+        print("Ошибка выполнения запроса:")
+        print(map_request)
+        print("Http статус:", response.status_code, "(", response.reason, ")")
+        sys.exit(1)
+
+    map_file = "map.png"
+    with open(map_file, "wb") as file:
+        file.write(response.content)
+
+def arrow_moves:
+    global scale, map_file
+
+
+if __name__ == '__main__':
+    map_file = "map.png"
+    scale = 43
+    get_pic()
+    pygame.init()
+    pygame.display.set_caption('map')
+    size = width, height = 600, 450
+    screen = pygame.display.set_mode(size)
+
+    running = True
+
+    while running:
+        for event in pygame.event.get():
+            # print(event)
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_PAGEDOWN:
+                if scale >= 6:
+                    scale -= 6
+                    get_pic()
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_PAGEUP:
+                plus_spn()
+
+            if event.type == pygame.
+
+        screen.blit(pygame.image.load(map_file), (0, 0))
+        pygame.display.flip()
+        screen.fill((0, 0, 0))
+    pygame.quit()
+    os.remove(map_file)
